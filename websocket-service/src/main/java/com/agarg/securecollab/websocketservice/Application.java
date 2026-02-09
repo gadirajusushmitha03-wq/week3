6b5685ae95fd092fe311980998a8393194e155e9
gadirajusushmitha03-wq <gadirajusushmitha03@gmail.com>
2026-02-09 15:42:47 +0530

    appl

--- websocket-service/src/main/java/com/agarg/securecollab/websocketservice/Application.java
+++ websocket-service/src/main/java/com/agarg/securecollab/websocketservice/Application.java
@@ -1,13 +1,15 @@
-
 package com.agarg.securecollab.websocketservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
 /**
  * websocket-service
  */
 @SpringBootApplication
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = ChatController.class))
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
