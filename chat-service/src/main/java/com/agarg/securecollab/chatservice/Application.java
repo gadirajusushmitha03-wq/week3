0bb6b83891245a4d6529b203905bdc9d43cc7b77
gadirajusushmitha03-wq <gadirajusushmitha03@gmail.com>
2026-02-09 15:17:41 +0530

    app

--- chat-service/src/main/java/com/agarg/securecollab/chatservice/Application.java
+++ chat-service/src/main/java/com/agarg/securecollab/chatservice/Application.java
@@ -3,11 +3,13 @@ package com.agarg.securecollab.chatservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * chat-service
  */
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.agarg.securecollab.chatservice.repository")
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- websocket-service/src/main/java/com/agarg/securecollab/websocketservice/ChatController.java
+++ websocket-service/src/main/java/com/agarg/securecollab/websocketservice/ChatController.java
@@ -1,15 +1,3 @@
 package com.agarg.securecollab.websocketservice;
 
-import org.springframework.messaging.handler.annotation.*;
-import org.springframework.stereotype.Controller;
-
-// DISABLED: Using EnhancedChatController instead
-// @Controller
-public class ChatController {
-
-    @MessageMapping("/chat.send")
-    @SendTo("/topic/messages")
-    public String send(String encryptedPayload) {
-        return encryptedPayload; // server never decrypts
-    }
-}
+// DISABLED: Using EnhancedChatController instead - this class is kept but not used
