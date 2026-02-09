face42052bba0ff0cab03c0c3c2c8e61d23a26fa
gadirajusushmitha03-wq <gadirajusushmitha03@gmail.com>
2026-02-09 21:35:20 +0530

    Add explicit deleteByRecipientId method to OfflineMessageRepository

--- chat-service/src/main/java/com/agarg/securecollab/repository/OfflineMessageRepository.java
+++ chat-service/src/main/java/com/agarg/securecollab/repository/OfflineMessageRepository.java
@@ -2,6 +2,9 @@ package com.agarg.securecollab.repository;
 
 import com.agarg.securecollab.chatservice.entity.OfflineMessageEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.time.LocalDateTime;
 
 public interface OfflineMessageRepository extends JpaRepository<OfflineMessageEntity, Long> {
+    void deleteByRecipientId(String recipientId);
+    void deleteByExpiresAtBefore(LocalDateTime dateTime);
 }
