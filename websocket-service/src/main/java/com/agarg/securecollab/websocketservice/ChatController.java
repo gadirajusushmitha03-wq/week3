40dbe5413e6a0b949b31ca24220ace05115a7374
gadirajusushmitha03-wq <gadirajusushmitha03@gmail.com>
2026-02-09 17:17:29 +0530

    gatewayfix

--- websocket-service/src/main/java/com/agarg/securecollab/websocketservice/ChatController.java
+++
@@ -1,4 +0,0 @@
-/*
- * DISABLED FILE - All WebSocket chat functionality has been moved to EnhancedChatController.java
- * This file is intentionally left empty to prevent Spring from scanning it.
- */
